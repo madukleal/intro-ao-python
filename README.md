5ea28a8683fa46106293a0a5318efe7bed308764
Maria Eduarda Leal <eduardakleal@gmail.com>
2022-05-02 17:58:51 -0300

    atualizando desafio 1 e incluindo nome no repositorio

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ConstruDelas - Introdução ao Python
-### Nome: 
+### Nome: Maria Eduada Kossatz Leal
 
 ## Visão Geral
 Bem vinda ao repositório do curso ConstruDelas, módulo de Introdução ao Python.
--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -1,3 +1,8 @@
 # 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
 
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
+
+name = input('vamos nos conhecer? insira seu nome:')
+age = input('qual a sua idade?:')
+city = input('onde você mora?:')
+print(f'oi {name}! seja bem vinde! você tem {age} anos e mora em {city}')
